50c0ee682b8e3005a502b71559e5efccf3798daa
kevtho1 <55199541+kevtho1@users.noreply.github.com>
2019-10-07 09:21:17 -0600

    Add files via upload

---
+++ ex04a.sql
@@ -0,0 +1,18 @@
+/* *** SQL-Query-CH03-01 *** */
+SELECT ProjectID, ProjectName, Department, MaxHours,
+
+StartDate, EndDate
+
+FROM project;
+/* *** SQL-Query-CH03-02 *** */
+SELECT *
+FROM project;
+/* *** SQL-Query-CH03-03 *** */
+SELECT ProjectName, Department, MaxHours
+FROM project;
+/* *** SQL-Query-CH03-04 *** */
+SELECT ProjectName, MaxHours, Department
+FROM project;
+/* *** SQL-Query-CH03-05 *** */
+SELECT Department
+FROM project;
